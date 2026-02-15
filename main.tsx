0ac8b60ba793146c9c15ccfaabbe21ac0f88b682
seihyen11-rgb <seihyen11@gmail.com>
2026-02-16 01:22:56 +0900

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css' // CSS 파일이 없다면 이 줄은 삭제해도 됩니다.
+import App from './App' // 같은 위치에 있는 App.tsx를 불러옵니다
+
+// index.css 파일이 없다면 아래 줄은 삭제하거나 주석 처리하세요
+// import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
